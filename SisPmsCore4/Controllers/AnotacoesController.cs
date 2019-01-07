af8a1854140872be2ba28c965b87060177c2c9f0
dmevalneto <dmevalneto@outlook.com>
2019-01-07 18:53:52 -0200

    BackUp

---
+++ SisPmsCore4/Controllers/AnotacoesController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SisPmsCore4.Models;
+
+namespace SisPmsCore4.Controllers
+{
+    public class AnotacoesController : Controller
+    {
+        IHttpContextAccessor HttpContextAccessor;
+        public AnotacoesController(IHttpContextAccessor httpContextAcessor)
+        {
+            HttpContextAccessor = httpContextAcessor;
+        }
+
+
+        // GET: Anotacoes
+        public ActionResult Index()
+        {
+            Anotacoes objAnotacoes = new Anotacoes(HttpContextAccessor);
+            ViewBag.ListaAnotacoes = objAnotacoes.ListaAnotacoes();
+            return View();
+        }
+
+        // GET: Anotacoes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cargo/Create
+        public ActionResult Create()
+        {
+            ViewBag.ListaSetor = new Setor(HttpContextAccessor).ListaSetor();
+            return View();
+        }
+
+        // POST: Cargo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Anotacoes formulario)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                if (ModelState.IsValid)
+                {
+                    formulario.HttpContextAccessor = HttpContextAccessor;
+                    formulario.SalvarNovoRegistro();
+                    return RedirectToAction("Index");
+                }
+                ViewBag.ListaSetor = new Setor(HttpContextAccessor).ListaSetor();
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Erro = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: Anotacoes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Anotacoes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Anotacoes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Anotacoes/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- SisPmsCore4/Models/Anotacoes.cs
+++ SisPmsCore4/Models/Anotacoes.cs
@@ -2,6 +2,7 @@
 using SisPmsCore4.Util;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,8 +14,10 @@ namespace SisPmsCore4.Models
         public string Nota { get; set; }
         public int setor_idsetor { get; set; }
         public int usuario_idusuario { get; set; }
+        public string NomeSetor { get; set; }
+        public string Data { get; set; }
 
-        IHttpContextAccessor HttpContextAccessor;
+        public IHttpContextAccessor HttpContextAccessor;
 
         public Anotacoes()
         {
@@ -27,26 +30,46 @@ namespace SisPmsCore4.Models
             HttpContextAccessor = httpContextAcessor;
         }
 
-        //public List<Anotacoes> ListaAnotacoes()
-        //{
-        //    List<Anotacoes> lista = new List<Anotacoes>();
-        //    Anotacoes item;
+        public List<Anotacoes> ListaAnotacoes()
+        {
+            List<Anotacoes> lista = new List<Anotacoes>();
+            Anotacoes item;
 
-        //    string id_usuario_logado = HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado");
-        //    string sql = $"SELECT * FROM 9256_sispmscore.anotacoes_setor WHERE usuario_idusuario = {usuario_idusuario}";
-        //    DAL objDAL = new DAL();
-        //    DataTable dt = objDAL.RetDataTable(sql);
+            string id_usuario_logado = HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado");
+            string sql = " SELECT " +
+                " anotacoes_setor.idanotacoes, anotacoes_setor.nota , anotacoes_setor.setor_idsetor , anotacoes_setor.usuario_idusuario, anotacoes_setor.data, setor.nome " +
+                " FROM anotacoes_setor " +
+                " inner join setor on setor.idsetor = anotacoes_setor.setor_idsetor " +
+                $" where usuario_idusuario = {id_usuario_logado} ORDER BY data";
 
-        //    for (int i = 0; i < dt.Rows.Count; i++)
-        //    {
-        //        item = new Cargo();
-        //        item.idCargo = int.Parse(dt.Rows[i]["idcargo"].ToString());
-        //        item.Nome = dt.Rows[i]["nome"].ToString();
-        //        item.Descricao = dt.Rows[i]["descricao"].ToString();
-        //        lista.Add(item);
-        //    }
-        //    return lista;
+            //string sql = $"SELECT * FROM 9256_sispmscore.anotacoes_setor WHERE usuario_idusuario = {id_usuario_logado}";
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.RetDataTable(sql);
 
-        //}
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                item = new Anotacoes();
+                item.IdAnotacoes = int.Parse(dt.Rows[i]["idanotacoes"].ToString());
+                item.Nota = dt.Rows[i]["nota"].ToString();
+                item.setor_idsetor = int.Parse(dt.Rows[i]["setor_idsetor"].ToString());
+                item.usuario_idusuario = int.Parse(dt.Rows[i]["usuario_idusuario"].ToString());
+                item.NomeSetor = dt.Rows[i]["nome"].ToString();
+                item.Data = DateTime.Parse(dt.Rows[i]["data"].ToString()).ToString("dd/MM/yyy");
+                lista.Add(item);
+            }
+            return lista;
+
+        }
+
+        public void SalvarNovoRegistro()
+        {
+            var data = DateTime.Now.ToString("yyyy/MM/dd");
+            string id_usuario_logado = HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado");
+            string sql = $"INSERT INTO anotacoes_setor (nota, setor_idsetor, usuario_idusuario, data) VALUES ('{Nota}', {setor_idsetor}, {id_usuario_logado}, '{data}')";
+            DAL objDAL = new DAL();
+            objDAL.ExecutarComandoSQL(sql);
+
+
+        }
     }
 }
